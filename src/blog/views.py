db3a8c1253476e2a075044ae246c21ee1b0f51fa
ElimuStuart <stuartelimu@gmail.com>
2020-03-06 06:44:41 +0300

    add list, retrieve apis

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+class ArticleListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleDetailView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
