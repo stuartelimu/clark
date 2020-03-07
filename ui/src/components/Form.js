cabe88800ccebcaef734ca8589b3f85435eeb16e
ElimuStuart <stuartelimu@gmail.com>
2020-03-07 23:09:16 +0300

    change onSubmit to onFinish to handle form submission

--- ui/src/components/Form.js
+++ ui/src/components/Form.js
@@ -3,21 +3,44 @@ import React, { Component } from "react";
 import { Form, Input, Button } from "antd";
 
 class CustomForm extends Component {
+
+  handleSubmit = values => {
+    // event.preventDefault();
+
+    const title = values.title;
+    const content = values.content;
+
+    console.log("Ok");
+    console.log(title, content);
+  };
+
+  componentDidMount() {
+    console.log("Ok");
+  }
+
   render() {
     return (
-      <div>
-        <Form>
-          <Form.Item label="Title">
-            <Input placeholder="Put a title here" />
-          </Form.Item>
-          <Form.Item label="Content">
-            <Input placeholder="Create some content" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary">Submit</Button>
-          </Form.Item>
-        </Form>
-      </div>
+      <Form onFinish={this.handleSubmit}>
+        <Form.Item
+          name="title"
+          label="Title"
+          rules={[{ required: true, message: "Please put a title here!" }]}
+        >
+          <Input placeholder="Put a title here" />
+        </Form.Item>
+        <Form.Item
+          name="content"
+          label="Content"
+          rules={[{ required: true, message: "Please create some content!" }]}
+        >
+          <Input placeholder="Create some content" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     );
   }
 }
