055f9ec46b418c8249e66b73f143eaeecb3f109a
ElimuStuart <stuartelimu@gmail.com>
2020-03-07 03:34:07 +0300

    Create ArticleDetail component

---
+++ ui/src/containers/ArticleDetail.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import axios from "axios";
+import {Card} from "antd";
+
+
+class ArticleDetail extends Component {
+
+  state = {
+    article: {},
+  }
+
+  componentDidMount() {
+    const articleID = this.props.match.params.articleID;
+    axios.get(`http://127.0.0.1:8000/blog/${articleID}`)
+      .then(res => {
+        this.setState({
+          article: res.data
+        })
+        console.log(res.data)
+      })
+  }
+
+  render() {
+    return <Card title={this.state.article.title}>
+  <p>{this.state.article.content}</p>
+    </Card>;
+  }
+}
+
+export default ArticleDetail;
--- ui/src/routes.js
+++ ui/src/routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import {Route} from "react-router-dom";
 
 import ArticleList from "./containers/ArticleList"
+import ArticleDetail from "./containers/ArticleDetail"
+
 
 const BaseRouter = () => (
   <div>
     <Route exact path="/" component={ArticleList} />
-    <Route exact path="/:articleID" component={ArticleList} />
+    <Route exact path="/:articleID" component={ArticleDetail} />
 
   </div>
 );
