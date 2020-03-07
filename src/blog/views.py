96f596b61b553b49365f5a7d1a5302e19ba36708
ElimuStuart <stuartelimu@gmail.com>
2020-03-07 18:03:44 +0300

    create ArticleViewset

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,33 +1,41 @@
-from rest_framework.generics import (
-    ListAPIView, 
-    RetrieveAPIView, 
-    CreateAPIView,
-    UpdateAPIView,
-    DestroyAPIView,
-)
+# from rest_framework.generics import (
+#     ListAPIView, 
+#     RetrieveAPIView, 
+#     CreateAPIView,
+#     UpdateAPIView,
+#     DestroyAPIView,
+# )
+from rest_framework import viewsets
 from .models import Article
 from .serializers import ArticleSerializer
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleDeleteView(DestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing article instances.
+    """
     serializer_class = ArticleSerializer
-
-
-class ArticleDetailView(RetrieveAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleCreateView(CreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleUpdateView(UpdateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleDeleteView(DestroyAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
