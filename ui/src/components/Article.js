849ca1d30dd09c578d875baa92a95821e3b8f5f7
ElimuStuart <stuartelimu@gmail.com>
2020-03-07 03:06:02 +0300

    add configurations for django-cors-headers

--- ui/src/components/Article.js
+++ ui/src/components/Article.js
@@ -1,18 +1,7 @@
 import React from "react";
 import { List, Avatar } from "antd";
 import { MessageOutlined, LikeOutlined, StarOutlined } from "@ant-design/icons";
-const listData = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: "http://ant.design",
-    title: `ant design part ${i}`,
-    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-    description:
-      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
-    content:
-      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently."
-  });
-}
+
 const IconText = ({ icon, text }) => (
   <span>
     {React.createElement(icon, { style: { marginRight: 8 } })}
@@ -31,7 +20,7 @@ const Articles = props => {
         },
         pageSize: 3
       }}
-      dataSource={listData}
+      dataSource={props.data}
       footer={
         <div>
           <b>ant design</b> footer part
